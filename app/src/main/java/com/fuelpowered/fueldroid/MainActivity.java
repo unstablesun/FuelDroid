f2f0bd4b2282a3eb38bb6251e70df57c5dd17c53
unstablesun <unstablesun@gmail.com>
2015-12-18 14:42:41 -0800

    fuelbroadcastreceiver

--- app/src/main/java/com/fuelpowered/fueldroid/MainActivity.java
+++ app/src/main/java/com/fuelpowered/fueldroid/MainActivity.java
@@ -129,6 +129,117 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private fuelbroadcastreceiver mMessageReceiver = new fuelbroadcastreceiver() {
+
+        final Handler handler = new Handler();
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onReceive(Context context, String action, Map<String, Object> data) {
+            // gonna get something
+            if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_VG_LIST.toString())) {
+                Toast.makeText(getApplicationContext(), "Virtual Goods List", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_VG_ROLLBACK.toString())) {
+                Toast.makeText(getApplicationContext(), "Challenge Counts", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_NOTIFICATION_ENABLED.toString())) {
+                Toast.makeText(getApplicationContext(), "Notification Enabled", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_NOTIFICATION_DISABLED.toString())) {
+                Toast.makeText(getApplicationContext(), "Notification Disabled", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_SOCIAL_LOGIN_REQUEST.toString())) {
+                // call back to say we have done it
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Login Request", Toast.LENGTH_SHORT).show();
+                        fuel.instance().sdkSocialLoginCompleted(null);
+                    }
+                }, 100);
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_SOCIAL_INVITE_REQUEST.toString())) {
+                // call back to say we have done it
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Invite Request", Toast.LENGTH_SHORT).show();
+                        fuel.instance().sdkSocialInviteCompleted();
+                    }
+                }, 100);
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_SOCIAL_SHARE_REQUEST.toString())) {
+                // call back to say we have done it
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Share Request", Toast.LENGTH_SHORT).show();
+                        fuel.instance().sdkSocialShareCompleted();
+                    }
+                }, 100);
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_COMPETE_EXIT.toString())) {
+                Toast.makeText(getApplicationContext(), "Exit", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_COMPETE_FAIL.toString())) {
+                Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_COMPETE_MATCH.toString())) {
+                if (data != null) {
+                    // extract the info
+                    final String tournamentId = (String) data.get("tournamentID");
+                    final String matchId = (String) data.get("matchID");
+                    final long score = 43;
+                    // we should play a game here then call the result
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Match Exit", Toast.LENGTH_SHORT).show();
+                            Map<String, Object> matchResult = new HashMap<String, Object>();
+                            matchResult.put("tournamentID", tournamentId);
+                            matchResult.put("matchID", matchId);
+                            matchResult.put("score", score);
+                            matchResult.put("visualScore", Long.toString(score));
+
+                            fuelcompete.instance().submitMatchResult(matchResult);
+                            fuelcompeteui.instance().launch();
+                        }
+                    }, 100);
+                }
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_COMPETE_CHALLENGE_COUNT.toString())) {
+                Toast.makeText(getApplicationContext(), "Challenge Counts", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_COMPETE_TOURNAMENT_INFO.toString())) {
+                Toast.makeText(getApplicationContext(), "Tournament Info", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_IGNITE_EVENTS.toString())) {
+                Toast.makeText(getApplicationContext(), "Events", Toast.LENGTH_SHORT).show();
+                // iterate the list and gather the events.
+                List<Map<String, Object>> events = (List<Map<String, Object>>) data.get("events");
+                Log.d("events", events.toString());
+                for (Map<String, Object> event : events) {
+                    String activityID = (String) event.get("id");
+                    switch ((int) event.get("type")) {
+                        case 0:
+                            fuelignite.instance().getLeaderBoard(activityID);
+                            break;
+                        case 1:
+                            fuelignite.instance().getMission(activityID);
+                            break;
+                        case 2:
+                            fuelignite.instance().getQuest(activityID);
+                            break;
+                    }
+                }
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_IGNITE_LEADERBOARD.toString())) {
+                Map<String, Object> leaderBoard = (Map<String, Object>) data.get("leaderBoard");
+                Log.d("leaderBoard", leaderBoard.toString());
+                Toast.makeText(getApplicationContext(), "LeaderBoard", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_IGNITE_MISSION.toString())) {
+                Map<String, Object> mission = (Map<String, Object>) data.get("mission");
+                Log.d("mission", mission.toString());
+                Toast.makeText(getApplicationContext(), "Mission", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_IGNITE_QUEST.toString())) {
+                Map<String, Object> quest = (Map<String, Object>) data.get("quest");
+                Log.d("quest", quest.toString());
+                Toast.makeText(getApplicationContext(), "Quest", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(fuelbroadcasttype.FSDK_BROADCAST_IGNITE_JOIN_EVENT.toString())) {
+                Log.d("join event", data.toString());
+            }
+
+        }
+
+    };
 
 
 }
